3f38b9b17e0063c79e72aafed1cdbaeeac347aaf
dennyxbox890 <dennyxbox890@gmail.com>
2021-04-09 10:50:31 +0800

    update main

--- main.php
+++ main.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style-p.css">
+</head>
 <body>
+//from https://www.w3schools.com/html/html_forms.asp
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="a">
+  <input type="submit">
+</form>
 
 <?php
-echo "Hello World!";
-
-phpinfo();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //using htmlspecialchars 2 prevent hack
+    $a = htmlspecialchars($_REQUEST['a']);
+    if (empty($a)) {
+        echo "empty";
+    } else {
+        echo $a;
+    }
+}
 ?>
 
 </body>
